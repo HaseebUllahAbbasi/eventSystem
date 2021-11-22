ef00381eb34603b75868c2a8a234711657c0a2d0
Haseeb U <haseebabbasi00@gmail.com>
2021-11-22 15:11:27 +0500

    tasks , requests screens init

--- BackEnd/Routers/TasksRoutes.js
+++ BackEnd/Routers/TasksRoutes.js
@@ -2,5 +2,4 @@ const express = require('express');
 const { getAllTasks } = require('../controllers/TaskController');
 const router = express.Router();
 router.route('/allTasks').get(getAllTasks);
-
 module.exports = router;
--- eventPlan/screens/MyRequests.js
+++ eventPlan/screens/MyRequests.js
@@ -1,10 +1,16 @@
-import React from "react";
+import React , {useState}from "react";
 import { View, Text } from "react-native";
 const MyRequests = ()=>
 {
+    const [requests,setRequests] = useState([]);
+    
     return (
         <View>
             <Text> My Requests</Text>
+            {
+                requests.length != 0 &&  requests.map(item=> <Text> Item</Text>)
+                
+            }
         </View>
 
     )
